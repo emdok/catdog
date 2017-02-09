7d885ad58dd5b153aeba7b1de91ea5e622e095c2
Emily Dokken and Jasun Feddema <emily@>
2017-02-08 19:51:06 -0800

    allow picture to show up when li is clicked and remove when image is clicked - using jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
+
   $("button#meow").click(function() {
     $("ul#cat").prepend("<li>Meow!</li>");
     $("ul#cat").children("li").first().click(function() {
-      $(this).remove();
+      $("li").html('<img src="img/cat.jpg"/>');
+     $("#cat li img").click(function() {
+      $("li").remove();
+      });
     });
   });
 
   $("button#bark").click(function() {
     $("ul#dog").prepend("<li>Bark!</li>");
     $("ul#dog").children("li").first().click(function() {
-      $(this).remove();
+      $("li").html('<img src="img/dog.png">');
+    $("#dog li img").click(function() {
+     $("li").remove();
+     });
     });
   });
 });
